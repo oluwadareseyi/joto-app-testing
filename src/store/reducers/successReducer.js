a8ec30e95565d70886a38ecfe154473a52cd0c8f
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-05 12:41:31 +0100

    add redux

---
+++ src/store/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param  {Array} state - Array of guessed words
+ * @param  {Object} action - Action to be reduced
+ * @returns {boolean} - new success state
+ */
+export default (state, action) => {
+  return null;
+};
