d56b5a3e21d67f749b32a5cfad7204437df5e320
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-05 09:56:15 +0100

    add bootstrap CDN

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
 
-function App() {
-  return (
-    <div data-test="app-component" className="App">
-      <div></div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div data-test="app-component" className="App">
+        <h1>Jotto</h1>
+        <Congrats success={false} />
+        <GuessedWords
+          guessedWords={[
+            { guessedWord: "train", letterMatchCount: 3 },
+            { guessedWord: "agile", letterMatchCount: 1 },
+            { guessedWord: "party", letterMatchCount: 5 },
+          ]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
