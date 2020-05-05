5325fdd93ac6e4de88de2ffe2c99e520b4efc32b
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-05 16:43:35 +0100

    add code to pass false sucess test

--- src/Input.js
+++ src/Input.js
@@ -4,15 +4,31 @@ import { connect } from "react-redux";
 class Input extends Component {
   render() {
     return (
-      <div>
-        <div></div>
+      <div data-test="component-input">
+        {this.props.success ? null : (
+          <form className="form-inline">
+            <input
+              data-test="input-box"
+              className="mb-2 mx-sm-3"
+              type="text"
+              placeholder="Enter guess"
+            />
+            <button
+              data-test="submit-button"
+              className="btn btn-primary"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
