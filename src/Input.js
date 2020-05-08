b99b405d7e2b4ec48ae5056205d1161522d73b73
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-08 12:32:02 +0100

    add props test to input

--- src/Input.js
+++ src/Input.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import * as actions from "./store/actions";
 
 class Input extends Component {
   render() {
@@ -31,4 +32,4 @@ const mapStateToProps = ({ success }) => {
   return { success };
 };
 
-export default connect(mapStateToProps)(Input);
+export default connect(mapStateToProps, actions)(Input);
