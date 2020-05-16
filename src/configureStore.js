22fa2423e8b2a56316686034c3552ad621f4d70b
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-16 14:28:50 +0100

    add more comments for even better documentation

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,3 +1,5 @@
+// in this file, we configue the store to use all our middlewares in setting up the store, then we export the store to index.js where it will be used as the store we pass into our provider.
+
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./store/reducers";
--- src/store/actions/index.test.js
+++ src/store/actions/index.test.js
@@ -2,19 +2,23 @@ import moxios from "moxios";
 import { storeFactory } from "../../../test/testUtils";
 import { getSecretWord } from "./";
 
-describe("get secet word", () => {
+describe("get secret word", () => {
+  // Before the test is run, install Moxios to take over from the normal call to the server the action will make, in this case, the `getSecretWord` action.
   beforeEach(() => {
     moxios.install();
   });
-
+  // uninstall Moxios after the test is run.
   afterEach(() => {
     moxios.uninstall();
   });
 
   test("adds response word to state", async () => {
     const secretWord = "party";
+
+    // we create an instance of the store from the utils File and return the store.
     const store = storeFactory();
 
+    // we call moxios wait to make an async request to the most recent call i.e. the action we'll be dispatching. We then respond with what our normal server should respond with on success.
     moxios.wait(() => {
       const request = moxios.requests.mostRecent();
       request.respondWith({
@@ -23,9 +27,13 @@ describe("get secet word", () => {
       });
     });
 
+    // we dispatch the getSecretWord action asynchronously, and this will be the action that moxios will take as the most recent.
     await store.dispatch(getSecretWord());
+
+    // we get a piece of our state after the action has been called, and see if the secret word reducer is updated.
     const newState = store.getState();
 
+    // We test to expect the secretWord part of our global store to be the secret word gotten from the server response.
     expect(newState.secretWord).toBe(secretWord);
   });
 });
